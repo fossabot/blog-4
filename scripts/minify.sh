a1af62a79243b811cc5d4363266e1cc8d5756df7
Jonathan A. Schweder <jonathanschweder@gmail.com>
2018-06-05 16:44:00 +0000

    Corrected minification

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -4,15 +4,11 @@
     --collapse-boolean-attributes \
     --collapse-inline-tag-whitespace \
     --collapse-whitespace \
-    --no-include-auto-generated-tags \
     --minify-css \
     --minify-js \
     --minify-ur-ls \
-    --quote-character \' \
+    --quote-character \" \
     --remove-attribute-quotes \
-    --remove-comments \
-    --remove-empty-elements \
-    --remove-optional-tags \
     --remove-redundant-attributes \
     --remove-script-type-attributes \
     --remove-style-link-type-attributes \
